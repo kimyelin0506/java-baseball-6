4079f9155cd89d467c220fe51d374cf81763a469
김예린 <annalin1004@gmail.com>
2024-10-23 03:42:29 +0900

    랜덤 수 뽑기 수정

--- src/main/java/baseball/model/RandomPickData.java
+++ src/main/java/baseball/model/RandomPickData.java
@@ -6,21 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomPickData {
-    static private List<String> randomList = new ArrayList<>();
+    private List<String> randomList = new ArrayList<>();
 
     public RandomPickData(){
-        for(int i=0; i<3; i++){
-            this.randomList.add(randomPick());
-        }
+        randomPick();
     }
 
-    private String randomPick(){
-        while(true){
-            int random = Randoms.pickNumberInRange(1,9);
-            if(!randomList.contains(random)){
-                return String.valueOf(random);
+    private void randomPick(){
+        String random = "";
+        for(int i=0; i<3; i++){
+            while(true){
+                random = String.valueOf(Randoms.pickNumberInRange(1,9));
+                if(!randomList.contains(random)){
+                    break;
+                }
             }
+            this.randomList.add(random);
         }
+
     }
 
     public List<String> getRandomList(){
